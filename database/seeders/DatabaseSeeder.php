ca81f87b3669a8edbaa7908898fe4c350decd1c6
iicechyd <chayada.w@kkumail.com>
2025-03-18 19:41:03 +0700

    delete users seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(RoleSeeder::class);
-        $this->call(UserSeeder::class);
         $this->call(ActivityTypeSeeder::class);
         $this->call(ActivitySeeder::class);
         $this->call(TmssSeeder::class);
